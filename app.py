5047e35e1766359c9996a1824c25487a5ff27260
sahara0219 <sahara1821@outlook.com>
2025-07-14 20:16:20 -0600

    cambios de varios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 from datetime import datetime
 
@@ -16,7 +17,24 @@ start_time = st.slider(
 
 st.write("Start time:", start_time)
 
-import pandas as pd
-vehiculos = pd.read_csv("vehicles_us (1).csv") 
+
+vehiculos = pd.read_csv("vehicles_us (1).csv") #lectura del archivo csv
 st.title("vehiculos en estados unidos")
 st.dataframe(vehiculos)
+hist_button = st.button('Construir histograma')
+vehiculos = vehiculos.dropna() #eliminacion de los valores nulos 
+vehiculos.colums = vehiculos.columns.str.lower().str.replace(" ", "_") #cambio de nombre de las columnas
+
+st.title("Analisis de Vehiculos en Estados Unidos (2019-2020)") #titulo de la aplicacion
+
+st.sidebar.header("Opciones de visualizacion") #opcines de visualizacion
+st.write("Eeste es un analisis de los vehiculos en Estados Unidos durante los años 2019 y 2020, donde se analiza el tipo de combustible, el tipo de vehiculo, la marca y el modelo de los vehiculos.") # descripcion de la aplicascion 
+
+st.subheader("Datos de los vehiculos") #subtitulos de la aplicacion
+
+st.dataframe(vehiculos) #visualizacion de los datos en un dataframe 
+st.write("Tolal de vehiculos:", len(vehiculos)) #total de vehiculos 
+st.write("Total de marcas:", vehiculos['model'].nunique()) # total de marcas 
+st.write("Total de modelos:", vehiculos['model_year'].nunique()) #total de modelos 
+st.write("Total de tipos de combustible:", vehiculos['fuel'].nunique()) #total de tipos de combustible
+st.write("tipos de trasmision:", vehiculos['transmission'].nunique()) #total de tipos de trasmision
